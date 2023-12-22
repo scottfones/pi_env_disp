8404084c86c61c738761dd401a14faccaddb2b99
Scott Fones <scott.fones@gmail.com>
2023-12-22 15:44:59 -0500

    added detailed vs simple display. simple blocks of width 2

--- env_report.py
+++ env_report.py
@@ -3,23 +3,71 @@ import time
 from gpiozero import CPUTemperature
 from sense_hat import SenseHat
 
-sense = SenseHat()
 
-while True:
-    cpu = CPUTemperature()
-    cpu_temp = cpu.temperature
+def construct_inner(colour_bg: list[int], colour_fg: list[int]):
+    o = colour_bg
+    x = colour_fg
 
-    cpu_text_color = [25, 125, 25]
-    cpu_back_color = [0, 0, 0]
+    # fmt: off
+    ring = [
+        x, x, x, x, x, x, x, x,
+        x, o, o, o, o, o, o, x,
+        x, o, o, o, o, o, o, x,
+        x, o, o, x, x, o, o, x,
+        x, o, o, x, x, o, o, x,
+        x, o, o, o, o, o, o, x,
+        x, o, o, o, o, o, o, x,
+        x, x, x, x, x, x, x, x,
+    ]
 
-    if cpu_temp > 50 and cpu_temp < 60:
-        cpu_text_color = [140, 120, 0]
-    elif cpu_temp >= 60:
-        cpu_text_color = [125, 125, 125]
-        cpu_back_color = [100, 0, 0]
+    return ring
 
+
+def construct_mid(colour_bg: list[int], colour_fg: list[int]):
+    o = colour_bg
+    x = colour_fg
+
+    # fmt: off
+    ring = [
+        o, o, o, o, o, o, o, o,
+        o, x, x, x, x, x, x, o,
+        o, x, x, x, x, x, x, o,
+        o, x, x, o, o, x, x, o,
+        o, x, x, o, o, x, x, o,
+        o, x, x, x, x, x, x, o,
+        o, x, x, x, x, x, x, o,
+        o, o, o, o, o, o, o, o,
+    ]
+
+    return ring
+
+
+def construct_outer(colour_bg: list[int], colour_fg: list[int]):
+    o = colour_bg
+    x = colour_fg
+
+    # fmt: off
+    ring = [
+        x, x, x, x, x, x, x, x,
+        x, x, x, x, x, x, x, x,
+        x, x, o, o, o, o, x, x,
+        x, x, o, o, o, o, x, x,
+        x, x, o, o, o, o, x, x,
+        x, x, o, o, o, o, x, x,
+        x, x, x, x, x, x, x, x,
+        x, x, x, x, x, x, x, x,
+    ]
+
+    return ring
+
+
+def detailed_display(
+    colour_bg: list[int], colour_fg: list[int], cpu_temp: float, sense: SenseHat
+):
     sense.show_message(
-        f"{cpu_temp:.1f}", text_colour=cpu_text_color, back_colour=cpu_back_color
+        f"{cpu_temp:.1f}",
+        text_colour=colour_fg,
+        back_colour=colour_bg,
     )
 
     time.sleep(0.25)
@@ -32,4 +80,43 @@ while True:
     case_humid = sense.get_humidity()
     sense.show_message(f"{case_humid:.1f}", text_colour=[25, 100, 100])
 
-    time.sleep(30)
+
+def simple_display(colour_bg: list[int], colour_fg: list[int], sense: SenseHat):
+    secs = time.localtime(time.time()).tm_sec
+
+    ring = []
+    if secs < 20:
+        ring = construct_outer(colour_bg, colour_fg)
+    elif secs < 40:
+        ring = construct_mid(colour_bg, colour_fg)
+    else:
+        ring = construct_inner(colour_bg, colour_fg)
+
+    sense.set_pixels(ring)
+
+
+def main():
+    cpu = CPUTemperature()
+    sense = SenseHat()
+
+    while True:
+        colour_fg = [25, 125, 25]
+        colour_bg = [0, 0, 0]
+
+        cpu_temp = cpu.temperature
+        if cpu_temp > 50 and cpu_temp < 60:
+            colour_fg = [140, 120, 0]
+        elif cpu_temp >= 60:
+            colour_fg = [125, 125, 125]
+            colour_bg = [100, 0, 0]
+
+        if sense.stick.get_events():
+            detailed_display(colour_bg, colour_fg, cpu_temp, sense)
+        else:
+            simple_display(colour_bg, colour_fg, sense)
+
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
